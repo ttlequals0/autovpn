5e16aa5a60e492b9a064e1a5c4a7ccc7c9f5a724
TTLequals0 <dkrachtus@ttlequals0.com>
2017-02-10 21:33:15 -0800

    New - Added the ability to specify custom OpenVpn port -p flag. Fix - reworked how modified files are backed up and restored.

--- scripts/create_ec2.py
+++ scripts/create_ec2.py
@@ -7,7 +7,7 @@ keyname=sys.argv[1]
 instance_type=sys.argv[2]
 region=sys.argv[3]
 ami=sys.argv[4]
-
+port=sys.argv[5]
 if region:
     conn_region = boto.ec2.connect_to_region(region)
 else:
@@ -18,48 +18,51 @@ def auto_vpn(ami=ami,
                     key_name=keyname,
                    	group_name="vpn_2",
                     ssh_port="22",
-                    vpn_port="1194",
+                    vpn_port=port,
                     cidr="0.0.0.0/0",
                     tag="auto_vpn",
                     user_data=None):
 	
 
-	ec2 = conn_region 
+    ec2 = conn_region 
  
-	try:
-    		group = ec2.get_all_security_groups(groupnames=[group_name])[0]
-	except ec2.ResponseError, e:
-    		if e.code == 'InvalidGroup.NotFound':
-        		group = ec2.create_security_group(group_name,
+    try:
+        group = ec2.get_all_security_groups(groupnames=[group_name])[0]
+    except ec2.ResponseError, e:
+        if e.code == 'InvalidGroup.NotFound':
+            group = ec2.create_security_group(group_name,
                                                 'A group that allows VPN access')
-    		else:
-        		raise
-	try:   
-    		group.authorize('tcp',ssh_port,ssh_port,cidr)
-    		group.authorize('udp',vpn_port,vpn_port,cidr)
+            group.authorize('tcp',ssh_port,ssh_port,cidr)
+            group.authorize('udp',vpn_port,vpn_port,cidr)
+        else:
+            raise
 
-	except ec2.ResponseError, e:
-    		if e.code == 'InvalidPermission.Duplicate':
-                    """print ('Security group %s already exists') % group_name"""
-                else:
-        	   raise
+    if int(port) != int(1194):
+        try:
+            mgroup = ec2.get_all_security_groups(groupnames=[group_name])[0]
+            mgroup.authorize('udp',vpn_port,vpn_port,cidr)
+        except ec2.ResponseError, e:
+            if e.code == 'InvalidPermission.Duplicate':
+                '''fail here'''
+            else: 
+                raise
 
-	reservation = ec2.run_instances(ami,
-    	key_name=key_name,
-    	security_groups=[group_name],
-    	instance_type=instance_type,
-    	user_data=user_data)
-
-	instance = reservation.instances[0]
-	while instance.state != 'running':
-    		time.sleep(30)
-    		instance.update()
+    reservation = ec2.run_instances(ami,
+        key_name=key_name,
+        security_groups=[group_name],
+        instance_type=instance_type,
+        user_data=user_data)
+    
+    instance = reservation.instances[0]
+    while instance.state != 'running':
+        time.sleep(30)
+        instance.update()
 
         instance.add_tag(tag)
 
-        global host
-        host = instance.ip_address
-        print "%s" % host
+    global host
+    host = instance.ip_address
+    print "%s" % host
 	
 
 if __name__ == "__main__": 
