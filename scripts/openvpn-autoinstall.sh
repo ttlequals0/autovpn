b57e1a5ca0fcb87caa121503656591a8fa80c84b
TTLequals0 <dkrachtus@ttlequals0.com>
2017-09-09 14:27:10 -0700

    Set EASYRSA_CRL_DAYS to 3650 instead of the default use easy-rsa v3.0.3

--- scripts/openvpn-autoinstall.sh
+++ scripts/openvpn-autoinstall.sh
@@ -81,12 +81,12 @@ if [[ -d /etc/openvpn/easy-rsa/ ]]; then
 fi
 
 # Get easy-rsa
-wget -O ~/EasyRSA-3.0.1.tgz https://github.com/OpenVPN/easy-rsa/releases/download/3.0.1/EasyRSA-3.0.1.tgz
-tar xzf ~/EasyRSA-3.0.1.tgz -C ~/
-mv ~/EasyRSA-3.0.1/ /etc/openvpn/
-mv /etc/openvpn/EasyRSA-3.0.1/ /etc/openvpn/easy-rsa/
+wget -O ~/EasyRSA-3.0.3.tgz "https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.3/EasyRSA-3.0.3.tgz"
+tar xzf ~/EasyRSA-3.0.3.tgz -C ~/
+mv ~/EasyRSA-3.0.3/ /etc/openvpn/
+mv /etc/openvpn/EasyRSA-3.0.3/ /etc/openvpn/easy-rsa/
 chown -R root:root /etc/openvpn/easy-rsa/
-rm -rf ~/EasyRSA-3.0.1.tgz
+rm -rf ~/EasyRSA-3.0.3.tgz
 cd /etc/openvpn/easy-rsa/
 # Create the PKI, set up the CA, the DH params and the server + client certificates
 ./easyrsa init-pki
@@ -95,8 +95,9 @@ cd /etc/openvpn/easy-rsa/
 ./easyrsa build-server-full server nopass
 ./easyrsa build-client-full $CLIENT nopass
 ./easyrsa gen-crl
+EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
 # Move the stuff we need
-cp pki/ca.crt pki/private/ca.key pki/dh.pem pki/issued/server.crt pki/private/server.key /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn
+cp pki/ca.crt pki/private/ca.key pki/dh.pem pki/issued/server.crt pki/private/server.key pki/crl.pem /etc/openvpn
 # CRL is read with each client connection, when OpenVPN is dropped to nobody
 chown nobody:$GROUPNAME /etc/openvpn/crl.pem
 # Generate key for tls-auth
@@ -137,7 +138,7 @@ sed -i '/\<net.ipv4.ip_forward\>/c\net.ipv4.ip_forward=1' /etc/sysctl.conf
 if ! grep -q "\<net.ipv4.ip_forward\>" /etc/sysctl.conf; then
 	echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
 fi
-# Avoid an unneeded reboot
+# Avoid reboot
 echo 1 > /proc/sys/net/ipv4/ip_forward
 # Needed to use rc.local with some systemd distros
 if [[ "$OS" = 'debian' && ! -e $RCLOCAL ]]; then
