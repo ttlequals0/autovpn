959caba6115e4a79a4295cf0d4bb48ca0de9256f
TTLequals0 <dkrachtus@ttlequals0.com>
2019-10-20 16:40:47 -0500

    convert to python3

--- scripts/create_ec2.py
+++ scripts/create_ec2.py
@@ -3,56 +3,56 @@ import boto
 import boto.ec2
 import sys
 
-keyname=sys.argv[1]
-instance_type=sys.argv[2]
-region=sys.argv[3]
-ami=sys.argv[4]
-port=sys.argv[5]
+keyname = sys.argv[1]
+instance_type = sys.argv[2]
+region = sys.argv[3]
+ami = sys.argv[4]
+port = sys.argv[5]
 if region:
     conn_region = boto.ec2.connect_to_region(region)
 else:
     conn_region = boto.connect_ec2()
 
-def auto_vpn(ami=ami,
-                    instance_type=instance_type,
-                    key_name=keyname,
-                   	group_name="vpn_2",
-                    ssh_port="22",
-                    vpn_port=port,
-                    cidr="0.0.0.0/0",
-                    tag="auto_vpn",
-                    user_data=None):
-	
 
-    ec2 = conn_region 
- 
+def auto_vpn(ami=ami,
+             instance_type=instance_type,
+             key_name=keyname,
+             group_name="vpn_2",
+             ssh_port="22",
+             vpn_port=port,
+             cidr="0.0.0.0/0",
+             tag="auto_vpn",
+             user_data=None):
+
+    ec2 = conn_region
+
     try:
         group = ec2.get_all_security_groups(groupnames=[group_name])[0]
-    except ec2.ResponseError, e:
+    except ec2.ResponseError as e:
         if e.code == 'InvalidGroup.NotFound':
             group = ec2.create_security_group(group_name,
-                                                'A group that allows VPN access')
-            group.authorize('tcp',ssh_port,ssh_port,cidr)
-            group.authorize('udp',vpn_port,vpn_port,cidr)
+                                              'A group that allows VPN access')
+            group.authorize('tcp', ssh_port, ssh_port, cidr)
+            group.authorize('udp', vpn_port, vpn_port, cidr)
         else:
             raise
 
     if int(port) != int(1194):
         try:
             mgroup = ec2.get_all_security_groups(groupnames=[group_name])[0]
-            mgroup.authorize('udp',vpn_port,vpn_port,cidr)
-        except ec2.ResponseError, e:
+            mgroup.authorize('udp', vpn_port, vpn_port, cidr)
+        except ec2.ResponseError as e:
             if e.code == 'InvalidPermission.Duplicate':
                 '''fail here'''
-            else: 
+            else:
                 raise
 
     reservation = ec2.run_instances(ami,
-        key_name=key_name,
-        security_groups=[group_name],
-        instance_type=instance_type,
-        user_data=user_data)
-    
+                                    key_name=key_name,
+                                    security_groups=[group_name],
+                                    instance_type=instance_type,
+                                    user_data=user_data)
+
     instance = reservation.instances[0]
     while instance.state != 'running':
         time.sleep(30)
@@ -62,8 +62,8 @@ def auto_vpn(ami=ami,
 
     global host
     host = instance.ip_address
-    print "%s" % host
-	
+    print("%s" % host)
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     auto_vpn()
--- scripts/terminate.py
+++ scripts/terminate.py
@@ -2,20 +2,20 @@ import boto
 import boto.ec2
 import sys
 
-region=sys.argv[1]
-instances=sys.argv[2]
+region = sys.argv[1]
+instances = sys.argv[2]
 
 
 conn_region = boto.ec2.connect_to_region(region)
 
 
 def ec2_terminate():
-                    
-	ec2 = conn_region  
-	
-	ec2.terminate_instances(instance_ids=[instances])
-	print "Success"	
-	
 
-if __name__ == "__main__": 
+    ec2 = conn_region
+
+    ec2.terminate_instances(instance_ids=[instances])
+    print("Success")
+
+
+if __name__ == "__main__":
     ec2_terminate()
